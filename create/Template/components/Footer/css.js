832dfc4d3b24ad638be74ac6730a185f5a12395c
eibonlabs <>
2025-02-27 08:22:48 -0600

    css cleanup

--- create/Template/components/Footer/css.js
+++ create/Template/components/Footer/css.js
@@ -1,8 +1,9 @@
 const style = `
 c-footer {
-  background-color:#450c36;
-  color:#f76ed3;
-  padding:10px 20px;
+  background-color: #450c36;
+  border-top: 1px solid #741c6d;
+  color: #f76ed3;
+  padding: 10px 20px;
 }
 `;
 
